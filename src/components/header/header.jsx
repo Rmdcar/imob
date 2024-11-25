e484c12ea38e0d88afa46893b446ed6fa3691703
unknown <moraisdecarvalho@gmail.com>
2024-11-25 16:43:58 -0300

    altera imagem

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,10 +2,11 @@ import styles from './styles.module.css'
 import { Link } from 'react-router-dom'
 
 function Header (){
+    const imgID = "1i1n1B-HXh4s66ZnrTRh8_YOmRmrdN2zE"
     return (
         <>
         <section className={styles.firstsection}>
-        <Link to="/"><img src="../logo.png" alt="logo" className={styles.logo}/></Link>
+        <Link to="/"><img src={`https://drive.google.com/thumbnail?id=${imgID}&sz=w1000`} alt="logo" className={styles.logo}/></Link>
             
         <div>
         
