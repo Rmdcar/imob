993c9c5d5d9e93cfb9d3f031b16903662de3bf4f
unknown <moraisdecarvalho@gmail.com>
2024-11-25 16:49:17 -0300

    alteração imagem no footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,11 +2,12 @@ import styles from "./styles.module.css";
 import { FaFacebook, FaInstagram, FaWhatsapp} from 'react-icons/fa';
 
 const Footer = () => {
+  const imgID = "1i1n1B-HXh4s66ZnrTRh8_YOmRmrdN2zE"
   return (
     <>
       <section className={styles.firstsection}>
         <div className={styles.imagens}>
-          <img src="../logo.png" alt="logo" className={styles.logo} />
+          <img src={`https://drive.google.com/thumbnail?id=${imgID}&sz=w1000`} alt="logo" className={styles.logo} />
           <div className={styles.icones}>
           <FaFacebook size="1em" /> 
           <FaInstagram size="1em" />
