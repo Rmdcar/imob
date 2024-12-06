313adfe0d46bcfe66514fe11fdebaa433b2fe0fe
unknown <moraisdecarvalho@gmail.com>
2024-12-06 10:11:43 -0300

    alterações

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodeResolve()],
   build: {
     outDir: 'dist',
     rollupOptions: {
