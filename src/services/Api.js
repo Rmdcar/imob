9b4bb2a7276190c4e68f12a76e5ed6a170c4fd78
unknown <moraisdecarvalho@gmail.com>
2024-12-11 17:54:51 -0300

    alterações

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const urlApi = 'http://localhost:3000/';
+export const urlApi = 'https://imob-backend.vercel.app/';
 
 const Api = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://imob-backend.vercel.app/',
   headers:{
   'Content-Type': 'application/json'
   }
